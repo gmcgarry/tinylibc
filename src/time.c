14f349dc25105cb0d30c3f5da7c535118612da83
Gregory McGarry <gregory.mcgarry@dolby.com>
2025-11-28 11:23:43 +1100

    Some improvement to the time implementation.

--- src/time.c
+++ src/time.c
@@ -13,6 +13,33 @@
 #define SEC_PER_HOUR	3600
 #define SEC_PER_MIN	60
 
+static const char *WeekDays[7] = {
+	"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+};
+
+static const char *Months[12] = {
+	"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+	"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+static const uint8_t MonthDays[] = {
+	31,28,31,30,31,30,31,31,30,31,30,31
+};
+
+static int 
+weekday(int mday, int month, int year)
+{
+	mday += 1;
+	month += 1;
+
+	if (month <= 2) {
+		year--;
+		month += 12;
+	}
+
+	month -= 2;
+	return (mday + year + year / 4 - year / 100 + year / 400 + (31 * month) / 12) % 7;
+}
 
 int
 nanosleep(const struct timespec *req, struct timespec *rem)
@@ -28,11 +55,9 @@ time(time_t* timep)
 
 	gettimeofday(&tv, &tz);
 
-        time_t total = tv.tv_sec % SEC_PER_DAY;
+        time_t total = tv.tv_sec;
         total += tz.tz_dsttime * SEC_PER_HOUR;
         total -= tz.tz_minuteswest * SEC_PER_MIN;
-        // mod `hms` to ensure in positive range of [0...SEC_PER_DAY)
-        total = (total + SEC_PER_DAY) % SEC_PER_DAY;
 
 	if (timep)
 		*timep = total;
@@ -43,29 +68,36 @@ struct tm *
 localtime(const time_t *timep)
 {
 	static struct tm tm;
+	time_t t = *timep;
 
-	tm.tm_sec = *timep % SEC_PER_MIN;
-	tm.tm_min = (*timep % SEC_PER_HOUR) / SEC_PER_MIN;
-        tm.tm_hour = (*timep % SEC_PER_DAY) / SEC_PER_HOUR;
+	tm.tm_sec = t % SEC_PER_MIN;
+	tm.tm_min = (t % SEC_PER_HOUR) / SEC_PER_MIN;
+        tm.tm_hour = (t % SEC_PER_DAY) / SEC_PER_HOUR;
 
-	tm.tm_mday = 0; // TODO
-	tm.tm_wday = 0; // TODO
-	tm.tm_mon = 0; // TODO
-	tm.tm_year = 0; // TODO
-	tm.tm_yday = 0; // TODO
+	t /= SEC_PER_DAY;
+        tm.tm_year = (uint16_t)(1970 - 1900 + t / 1461 * 4);
+        t %= 1461;
+        unsigned int n = ((t >= 1096) ? t - 1 : t) / 365;
+        tm.tm_year += n;
+        t -= n * 365 + (n > 2 ? 1 : 0);
+
+	tm.tm_yday = t % 365;;
+
+        unsigned int i;
+        for (i = 0; i < 12; i++) {
+                uint32_t d = MonthDays[i];
+                if (i == 1 && n == 2) d++;
+                if (t < d) break;
+                t -= d;
+        }
+        tm.tm_mon = i;
+        tm.tm_mday = 1+t;
+	tm.tm_wday = weekday(t, i, tm.tm_year);
+
 	tm.tm_isdst = 0; // TODO
 
 	return &tm;
 }
-
-static const char *WeekDays[7] = {
-	"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-};
-
-static const char *Months[12] = {
-	"Jan", "Feb", "Mar", "Apr", "May", "Jun",
-	"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-};
 
 void
 asctime_r(const struct tm *tp, char *buf)
